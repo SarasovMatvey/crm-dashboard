ed0982199df023ca6529441c05ba58e8297f2d7c
SarasovMatvey <sarasovmatvej@gmail.com>
2021-05-27 14:00:29 +0600

    Updates

---
+++ js/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', e => {
+  console.log('Installed');
+});
+
+self.addEventListener('activate', e => {
+  console.log('Activated');
+});
+
+self.addEventListener('fetch', e => {
+  console.log('Fetching');
+});
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzdy5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJzZWxmLmFkZEV2ZW50TGlzdGVuZXIoJ2luc3RhbGwnLCBlID0+IHtcbiAgY29uc29sZS5sb2coJ0luc3RhbGxlZCcpO1xufSk7XG5cbnNlbGYuYWRkRXZlbnRMaXN0ZW5lcignYWN0aXZhdGUnLCBlID0+IHtcbiAgY29uc29sZS5sb2coJ0FjdGl2YXRlZCcpO1xufSk7XG5cbnNlbGYuYWRkRXZlbnRMaXN0ZW5lcignZmV0Y2gnLCBlID0+IHtcbiAgY29uc29sZS5sb2coJ0ZldGNoaW5nJyk7XG59KTtcbiJdLCJmaWxlIjoic3cuanMifQ==
